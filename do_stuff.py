e4f31e5da5c68d1f55210cd823780ef68d6361c4
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-09-20 16:22:15 -0400

    add test_do_stuff

---
+++ do_stuff.py
@@ -0,0 +1,19 @@
+def sum_list(number_list):
+    """sum all the numbers in number_list
+    """
+    import numpy as np
+
+    s = np.sum(number_list)
+
+    return s
+
+def min_max_list(number_list):
+    """find the min/max of the list
+    """
+    import numpy as np
+
+    d = np.array(number_list)
+    mi = d.min()
+    ma = d.max()
+
+    return (mi, ma)
